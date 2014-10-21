5130cb793027b27a9d9e985b4e25f0f634fbaf21
pedrolima1990 <pedro_lima_18@hotmail.com>
2014-10-21 15:47:34 +0100

    Adicionado botão na SettingsActivity para o utilizador indicar que já escolheu as turmas que pretende.

--- NewsClass/src/com/example/newsclass/SettingsActivity.java
+++ NewsClass/src/com/example/newsclass/SettingsActivity.java
@@ -3,6 +3,9 @@ package com.example.newsclass;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -11,12 +14,25 @@ public class SettingsActivity extends Activity{
 
 	private TextView _tv2;
 	private ListView _listView2;
+	private CustomAdapter adapter;
 	public SharedPreferences _pref;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings_layout);
+		
+		Button btn = (Button) findViewById(R.id.button1);
+		btn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				_pref.edit()
+				.putStringSet("ids", adapter.getSetListIds())
+				.commit();
+				SettingsActivity.this.finish();
+			}
+			}
+		);          
+		
 		
 		_tv2 = (TextView) findViewById(R.id.tv2);
 		_listView2 = (ListView) findViewById(R.id.ListView2);
@@ -32,11 +48,7 @@ public class SettingsActivity extends Activity{
 				}else {
 					CustomAdapter adapter = new CustomAdapter(SettingsActivity.this, R.layout.item_layout, result); 
 					_listView2.setAdapter(adapter);
-					_listView2.setOnScrollListener(adapter);
-					
-					_pref.edit()
-						.putStringSet("ids", adapter.getSetListIds())
-						.commit();				
+					_listView2.setOnScrollListener(adapter);			
 				}
 			}
 		};
