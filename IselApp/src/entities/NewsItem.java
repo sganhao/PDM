0b7f1b5fed0f256bdded9bd0a7f4873b7e53f9d2
pedrolima1990 <pedro_lima_18@hotmail.com>
2015-01-03 18:17:27 +0000

    criada a classe requestsToThoth para efectuar os pedidos http ao thoth. criado o construtor na classe WorkItem. efectuadas algumas alterações na classe NewsItem.

--- IselApp/src/entities/NewsItem.java
+++ IselApp/src/entities/NewsItem.java
@@ -1,6 +1,8 @@
 package entities;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class NewsItem implements Serializable{
@@ -13,13 +15,15 @@ public class NewsItem implements Serializable{
 	public String news_content;
 	public boolean news_isViewed;
 	
-	public NewsItem (String news_fullname, int news_newsId, int news_id, String news_title, Date news_when, String news_content, boolean news_isViewed) {
+	public NewsItem (String news_fullname, int news_newsId, int news_id, String news_title, String news_when, String news_content, boolean news_isViewed) throws ParseException {
 		this.news_fullname = news_fullname;
 		this.news_newsId = news_newsId;
 		this.news_id = news_id;
 		this.news_title = news_title;
-		this.news_when = news_when;
 		this.news_content = news_content;
 		this.news_isViewed = news_isViewed;
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		this.news_when = format.parse(news_when);
 	}
 }
